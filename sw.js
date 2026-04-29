67d6b6cfe198187f1ae35a4870ee570c3d0c675c
nymka3015 <nymka.3015@gmail.com>
2026-04-29 17:23:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker installed');
+});
 
+self.addEventListener('fetch', (e) => {
+  // Энэ нь вэбийг офлайн үед ажиллахад тусалдаг, одоохондоо хоосон байж болно
+});
